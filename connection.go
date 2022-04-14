c188213c9f060847534a568f9c9a0a44c1ee059a
Toby <tobyxdd@gmail.com>
2022-04-14 00:41:55 -0700

    ErrMessageToLarge

--- connection.go
+++ connection.go
@@ -2142,14 +2142,21 @@ func (s *connection) onStreamCompleted(id protocol.StreamID) {
 	}
 }
 
+type ErrMessageToLarge int
+
+func (e ErrMessageToLarge) Error() string {
+	return fmt.Sprintf("message too large (max %d bytes)", e)
+}
+
 func (s *connection) SendMessage(p []byte) error {
 	if !s.supportsDatagrams() {
 		return errors.New("datagram support disabled")
 	}
 
 	f := &wire.DatagramFrame{DataLenPresent: true}
-	if protocol.ByteCount(len(p)) > f.MaxDataLen(s.peerParams.MaxDatagramFrameSize, s.version) {
-		return errors.New("message too large")
+	maxDataLen := f.MaxDataLen(s.peerParams.MaxDatagramFrameSize, s.version)
+	if protocol.ByteCount(len(p)) > maxDataLen {
+		return ErrMessageToLarge(maxDataLen)
 	}
 	f.Data = make([]byte, len(p))
 	copy(f.Data, p)
