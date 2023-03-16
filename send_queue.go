0e3920d4fe5b972740ecea5014cfbf8f6e34f5d4
wwqgtxx <wwqgtxx@gmail.com>
2023-03-16 20:38:25 +0800

    handle quic-go's ErrMesageTooLarge in sendQueue too

--- send_queue.go
+++ send_queue.go
@@ -1,6 +1,10 @@
 package quic
 
-import "github.com/quic-go/quic-go/internal/protocol"
+import (
+	"errors"
+
+	"github.com/quic-go/quic-go/internal/protocol"
+)
 
 type sender interface {
 	Send(p *packetBuffer, gsoSize uint16, ecn protocol.ECN)
@@ -83,7 +87,8 @@ func (h *sendQueue) Run() error {
 				// 1. Checking for "datagram too large" message from the kernel, as such,
 				// 2. Path MTU discovery,and
 				// 3. Eventual detection of loss PingFrame.
-				if !isSendMsgSizeErr(err) {
+				var tooLarge ErrMessageTooLarge
+				if !isSendMsgSizeErr(err) && !errors.As(err, &tooLarge) {
 					return err
 				}
 			}
