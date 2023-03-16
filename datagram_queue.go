f305031e82d7e3ffc8a486f8458faa915b89cc30
wwqgtxx <wwqgtxx@gmail.com>
2023-03-16 20:37:32 +0800

    add DatagramFrameMaxPeekTimes to avoid big packet hang whole connection

--- datagram_queue.go
+++ datagram_queue.go
@@ -8,9 +8,12 @@ import (
 	"github.com/metacubex/quic-go/internal/wire"
 )
 
+const DatagramFrameMaxPeekTimes = 10
+
 type datagramQueue struct {
 	sendQueue chan *wire.DatagramFrame
 	nextFrame *wire.DatagramFrame
+	peekTimes int
 
 	rcvMx    sync.Mutex
 	rcvQueue [][]byte
@@ -59,7 +62,15 @@ func (h *datagramQueue) AddAndWait(f *wire.DatagramFrame) error {
 // If actually sent out, Pop needs to be called before the next call to Peek.
 func (h *datagramQueue) Peek() *wire.DatagramFrame {
 	if h.nextFrame != nil {
-		return h.nextFrame
+		h.peekTimes++
+		if h.peekTimes > DatagramFrameMaxPeekTimes {
+			if h.logger.Debug() {
+				h.logger.Debugf("Discarded DATAGRAM frame (%d bytes payload)", len(h.nextFrame.Data))
+			}
+			h.nextFrame = nil
+		} else {
+			return h.nextFrame
+		}
 	}
 	select {
 	case h.nextFrame = <-h.sendQueue:
@@ -67,6 +78,7 @@ func (h *datagramQueue) Peek() *wire.DatagramFrame {
 	default:
 		return nil
 	}
+	h.peekTimes = 0
 	return h.nextFrame
 }
 
