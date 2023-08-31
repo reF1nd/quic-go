fc9f378af913f4932f40485c34f9a9e6525f3105
世界 <i@sekai.icu>
2023-08-31 11:09:00 +0800

    Make interface shared

--- connection.go
+++ connection.go
@@ -443,7 +443,7 @@ var newClientConnection = func(
 	}
 	if s.config.TokenStore != nil {
 		if token := s.config.TokenStore.Pop(s.tokenStoreKey); token != nil {
-			s.packer.SetToken(token.data)
+			s.packer.SetToken(token.Data)
 		}
 	}
 	return s
@@ -1484,7 +1484,7 @@ func (s *connection) handleNewTokenFrame(frame *wire.NewTokenFrame) error {
 		}
 	}
 	if s.config.TokenStore != nil {
-		s.config.TokenStore.Put(s.tokenStoreKey, &ClientToken{data: frame.Token})
+		s.config.TokenStore.Put(s.tokenStoreKey, &ClientToken{Data: frame.Token})
 	}
 	return nil
 }
