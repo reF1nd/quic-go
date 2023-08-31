d8e7d2e8236d844ba0bd7af34cbba32d937132c7
世界 <i@sekai.icu>
2023-08-31 11:09:00 +0800

    Prepare package copies for ECH

--- ech/server.go
+++ ech/server.go
@@ -10,7 +10,7 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/quic-go/quic-go/internal/handshake"
+	"github.com/quic-go/quic-go/internal/handshake_ech"
 	"github.com/quic-go/quic-go/internal/protocol"
 	"github.com/quic-go/quic-go/internal/qerr"
 	"github.com/quic-go/quic-go/internal/utils"
