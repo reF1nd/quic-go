eeaeecd0b3c9ab73599a11826a2f3e062cc1d71a
世界 <i@sekai.icu>
2023-08-31 11:09:00 +0800

    Prepare package copies for ECH

--- ech/crypto_stream_manager.go
+++ ech/crypto_stream_manager.go
@@ -3,7 +3,7 @@ package quic
 import (
 	"fmt"
 
-	"github.com/quic-go/quic-go/internal/handshake"
+	"github.com/quic-go/quic-go/internal/handshake_ech"
 	"github.com/quic-go/quic-go/internal/protocol"
 	"github.com/quic-go/quic-go/internal/wire"
 )
