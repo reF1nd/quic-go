1dbf4ed63e97e612ef0004fb5514270014b2e9c8
世界 <i@sekai.icu>
2023-09-19 17:54:53 +0800

    Add ech_create_copies.sh

---
+++ ech_create_copies.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+mkdir ech
+cp ./*.go ech
+cp -r http3 http3_ech
+cp -r internal/handshake internal/handshake_ech
+cp -r internal/qtls internal/qtls_ech
+git add .
+git commit -m "Add package copies for ECH"
